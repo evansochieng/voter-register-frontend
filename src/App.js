7fd62a0c253d9acec21ddb53e3bffe0bd523bcff
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-11-09 12:59:45 +0300

    styled the navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddVoter from "./pages/AddVoter";
 import NavBar from "./pages/NavBar";
 import PollingStationsList from "./pages/PollingStationList";
 import Home from "./pages/Home";
+import "./nav.css"
 
 
 function App() {
