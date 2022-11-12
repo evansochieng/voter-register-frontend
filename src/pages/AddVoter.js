4a4b9a4d31317a6d434fc4a0b71760a490582687
charity <wa.nelimacharity@gmail.com>
2022-11-12 18:58:02 +0300

    more changes

--- src/pages/AddVoter.js
+++ src/pages/AddVoter.js
@@ -66,18 +66,18 @@ const AddVoter = ({ stationData }) => {
                     <label>ID Number</label><br />
                     <input type="text" placeholder="Enter ID..." name="id_number" onChange={handleChange}></input>                          
                 </div>
-                <div>
+                <div >
                     <label>D.O.B</label><br />
-                    <input type="date" min="01/11/2004" name="DOB" value={record.DOB} onChange={handleChange}></input>                          
+                    <input type="date" max="2004-11-01" name="DOB" value={record.DOB} onChange={handleChange}></input>                          
                 </div>
                 <div>
                     <label>Age</label><br />
                     <input type="text" placeholder='Add age...' name="age" value={record.age} onChange={handleChange}></input>                          
                 </div>
-                <div>
+                <div >
                     <label>Gender</label><br/>
                     <select name="gender" value={record.gender} onChange={handleChange}>
-                        <option>-select-</option>
+                        <option className='gender'>-select-</option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
                     </select>                       
